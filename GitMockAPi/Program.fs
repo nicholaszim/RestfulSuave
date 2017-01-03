6880403255ff24d2edb5cc566a3ed7c42940a349
Nick <zinomast@gmail.com>
2017-01-04 01:10:54 +0200

    Git Mock api

---
+++ GitMockAPi/Program.fs
@@ -0,0 +1,19 @@
+﻿open Suave
+open Suave.Operators
+open Suave.Successful
+open Suave.Filters
+open System.IO
+
+[<EntryPoint>]
+let main argv = 
+    let json fileName =
+        let content = File.ReadAllText fileName
+        content.Replace("\r", "").Replace("\n", "")
+        |> OK >=> Writers.setMimeType "application/json"
+
+    let user = pathScan "/users/%s" (fun _ -> "User.json" |> json)
+    let repos = pathScan "/users/%s/repos" (fun _ -> "Repos.json" |> json)
+    let mockAPi = choose [repos; user]
+
+    startWebServer defaultConfig mockAPi
+    0 // return an integer exit code
